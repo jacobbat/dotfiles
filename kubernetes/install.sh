0b42a17fe5cdf510ed226c21c4b8e43d9f419a5e
Chris Hirsch <chris@base2technology.com>
2020-06-02 12:16:51 -0600

    Added helm

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,2 +1,4 @@
 # Install diff for helm
 helm plugin install https://github.com/databus23/helm-diff --version master
+# Need to install secret handler stuff
+helm plugin install https://github.com/futuresimple/helm-secrets
